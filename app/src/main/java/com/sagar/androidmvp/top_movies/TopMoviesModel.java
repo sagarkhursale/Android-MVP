13a5cf04d3a8e70f32f3983326b1717ec6cb460a
sagar khursale <sagar.khursale@webmatrixtech.com>
2018-10-06 02:28:03 +0530

    Application Module

---
+++ app/src/main/java/com/sagar/androidmvp/top_movies/TopMoviesModel.java
@@ -0,0 +1,15 @@
+package com.sagar.androidmvp.top_movies;
+
+import io.reactivex.Observable;
+
+public class TopMoviesModel implements TopMoviesActivityMvp.Model{
+
+
+
+    @Override
+    public Observable<ViewModel> result() {
+        return null;
+    }
+
+//
+}
