12571e3614a6d1a0f4a0370720513bb1440161f1
sagar khursale <sagar.khursale@webmatrixtech.com>
2018-10-06 03:26:49 +0530

    Api module for Name

---
+++ app/src/main/java/com/sagar/androidmvp/http/ApiModuleForName.java
@@ -0,0 +1,59 @@
+package com.sagar.androidmvp.http;
+
+import android.support.annotation.NonNull;
+import java.io.IOException;
+import dagger.Provides;
+import okhttp3.HttpUrl;
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+
+public class ApiModuleForName {
+
+    private final String BASE_URL="https://api.themoviedb.org/3/movie/";
+    private final String API_KEY="ef11f8a97d50d37a71e4353b2fb086a6";
+
+
+    @Provides
+    public OkHttpClient provideClient() {
+        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BASIC);
+
+        return new OkHttpClient.Builder().addInterceptor(interceptor).addInterceptor(new Interceptor() {
+            @Override
+            public Response intercept(@NonNull Chain chain) throws IOException {
+                Request request = chain.request();
+                HttpUrl url = request.url().newBuilder().addQueryParameter("api_key", API_KEY).build();
+                request = request.newBuilder().url(url).build();
+                return chain.proceed(request);
+            }
+        }).build();
+    }
+
+
+    @Provides
+    public Retrofit provideRetrofit(String baseURL, OkHttpClient client) {
+        return new Retrofit.Builder()
+                .baseUrl(baseURL)
+                .client(client)
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+    }
+
+
+    @Provides
+    public MovieApiService provideApiService()
+    {
+        return provideRetrofit(BASE_URL,provideClient()).create(MovieApiService.class);
+    }
+
+
+    // END
+}
