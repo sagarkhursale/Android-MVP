13a06a582a5d287f2641f2231c865014816e3b4d
sagar khursale <sagar.khursale@webmatrixtech.com>
2018-10-06 00:14:58 +0530

    bind views

--- app/src/main/java/com/sagar/androidmvp/MainActivity.java
+++ app/src/main/java/com/sagar/androidmvp/MainActivity.java
@@ -2,12 +2,33 @@ package com.sagar.androidmvp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.recycler_view)
+    private RecyclerView recyclerView;
+
+    @BindView(R.id.listActivity_rootView)
+    private ViewGroup rootView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+
+
+        // end
     }
+
+
+
+    // END
 }
